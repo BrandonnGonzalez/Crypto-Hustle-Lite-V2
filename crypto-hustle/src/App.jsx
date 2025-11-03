70e0576399bfe6d36e22b8cf0114e8738bd012dd
Brandon Gonzalez <brandonyt@icloud.com>
2025-11-03 18:16:43 -0500

    Creating a search bar and utilizing conditional rendering to search and filter for specific coins

--- crypto-hustle/src/App.jsx
+++ crypto-hustle/src/App.jsx
@@ -7,6 +7,24 @@ const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
   const [list, setList] = useState(null);
+  const [filteredResults, setFilteredResults] = useState([])
+  const [searchInput, setSearchInput] = useState("")
+
+  const searchItems = searchValue => {
+    setSearchInput(searchValue)
+    if (searchValue !== "") {
+      const filteredData = list.Data.filter((item) => 
+        Object.values(item.CoinInfo)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData)
+    } else {
+      setFilteredResults(list.Data)
+    }
+  }
+
 
   useEffect(() => {
     const fetchAllCoinData = async () => {
@@ -23,6 +41,49 @@ function App() {
   return (
     <div className="whole-page">
       <h1>My Crypto List</h1>
+      <input
+      type="text"
+      placeholder="Search..."
+      onChange={(inputString) => searchItems(inputString.target.value)} 
+      />
+
+      {searchInput.length > 0
+        ? filteredResults
+            .map((coin) => {
+              const coinData = coin.CoinInfo;
+              if (
+                coinData.Algorithm !== "N/A" &&
+                coinData.ProofType !== "N/A"
+              ) {
+                return (
+                  <CoinInfo
+                    key={coinData.Name}
+                    image={coinData.ImageUrl}
+                    name={coinData.FullName}
+                    symbol={coinData.Name}
+                  />
+                );
+              }
+              return null;
+            })
+        : list?.Data
+            .map((data) => data.CoinInfo)
+            .filter(
+              (coinData) =>
+                coinData.Algorithm !== "N/A" &&
+                coinData.ProofType !== "N/A"
+            )
+            .map((coinData) => (
+              <CoinInfo
+                key={coinData.Name}
+                image={coinData.ImageUrl}
+                name={coinData.FullName}
+                symbol={coinData.Name}
+              />
+            ))}
+
+
+      
       <ul>
         {list?.Data.map((data) => data.CoinInfo)
           .filter(
